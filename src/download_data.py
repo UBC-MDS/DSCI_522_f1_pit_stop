4ee9a44c0bf72229517a47dcbc804ccb2d4a7157
florawendy19 <90071253+florawendy19@users.noreply.github.com>
2021-12-13 18:01:10 -0800

    change download.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,28 +1,20 @@
 # author: Flora Ouedraogo
 # date: 2021-11-20
 
-"""This script will download pit_stops.csv from the web to a local filepath as either a csv or feather file format.
+"""The following is the steps for downloading and unzipping the csv file
 
-Usage: src/down_data.py --url=<url> --out_file=<out_file>
 
 Options:
 --url=<url>              URL from where to download the data (must be in standard csv format)
 --out_file=<out_file>    Path (including filename) of where to locally write the file
 """
-#comment
-from docopt import docopt
-import pandas as pd
-import os
 
-opt = docopt(__doc__)
-
-def main(url, out_file):
-
-  data = pd.read_csv(url)
-
-  os.makedirs(os.path.dirname(out_file))
-
-   data.to_csv(out_file, index = False)
-
-if __name__ == "__main__":
-  main(opt["--url"], opt["--out_file"])
+import urllib.request
+pdf_path = ""
+def download_file(download_url, filename):
+    response = urllib.request.urlopen(download_url)    
+    file = open(filename + ".pdf", 'wb')
+    file.write(response.read())
+    file.close()
+ 
+download_file(pdf_path, "Test")
